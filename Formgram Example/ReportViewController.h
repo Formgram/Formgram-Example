983de5cc941b270f298a1dde807c63047b3a3c4d
Formgram <henry@formgram.com>
2015-02-22 23:34:18 -0800

    Added new fields to the example form
    Added new fields to the example form to show how you can add a submit
    button and a link.  In this release, the link points to a screen
    showing all the form entries anyone has submitted for this specific
    form.

---
+++ Formgram Example/ReportViewController.h
@@ -0,0 +1,16 @@
+
+//  Formgram Example
+//
+//  Created by Henry Situ on 2/21/15.
+//  Copyright (c) 2015 Henry Situ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReportViewController : UIViewController  <NSXMLParserDelegate>
+
+@property (strong, nonatomic) IBOutlet UIWebView *formgramReportWebView;
+
+
+@end
+
